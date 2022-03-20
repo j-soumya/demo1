7daeb26075b6dcbf740bbe28949bb68d359f2e0a
Soumya Jain <77jainsoumya@gmail.com>
2022-03-20 19:08:14 +0530

    Added new file-9

---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,11 @@
+// since sending props, need to catch it here
+const ChatFeed = (props) => 
+{
+    console.log(props);
+    return(
+        <div>ChatFeed</div>
+    )
+
+}
+
+export default ChatFeed;
